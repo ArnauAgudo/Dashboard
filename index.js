e66dc0239c82fe88f9f8ca9e28ca076c615d60d4
ArnauAgudo <159519789+ArnauAgudo@users.noreply.github.com>
2025-05-20 20:32:31 +0000

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+  
+  const divEmails = document.querySelectorAll(".divemail");
+
+  for (let i = 0; i < divEmails.length; i++) {
+    divEmails[i].addEventListener("click", function () {
+      
+      divEmails[i].classList.toggle("seleccionado");
+    });
+  }
